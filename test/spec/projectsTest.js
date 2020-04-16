e51a945f152ce12852a202aa8ce7f938f89f98a9
vn03iim <vn03iim@mx.walmart.com>
2020-04-16 12:46:53 -0500

    Adding performance files

---
+++ test/spec/projectsTest.js
@@ -0,0 +1,22 @@
+import { } from 'dotenv/config'
+import chai from 'chai'
+import chaitHttp from 'chai-http'
+
+describe('Projects API ENDPOINTS Tests',() => {
+    const AUTH_TOKEN = process.env.API_TOKEN
+    const PROJECTS_PATH='/projects' 
+  beforeEach(()=>{
+      chai.use(chaitHttp)
+      chai.should()
+  })
+it('Get ALL Projects', () => {
+        chai.request(process.env.URL_BASE)
+        .get(PROJECTS_PATH)
+        .set('Authorization','Bearer '+AUTH_TOKEN)
+        .end((err,res) => {
+            res.should.have.status(200)
+            console.log(res.body[0].name)
+            res.body[0].name.should.be.equal('Inbox')
+        })
+    })
+})
